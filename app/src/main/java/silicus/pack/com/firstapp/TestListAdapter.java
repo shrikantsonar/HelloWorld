d277a1dfe5cad43d545492b9daf19452ac9849d1
Shrikant Sonar <shrikant.sonar@silicus.com>
2015-09-29 14:37:43 +0530

    Added adapter class.

---
+++ app/src/main/java/silicus/pack/com/firstapp/TestListAdapter.java
@@ -0,0 +1,39 @@
+package silicus.pack.com.firstapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+
+import junit.framework.Test;
+
+/**
+ * Created by SVSonar on 9/29/2015.
+ */
+public class TestListAdapter extends BaseAdapter {
+
+    public TestListAdapter() {
+        super();
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
